fc8832429a92bd22fd8907a27e54756e9d673760
LeidyRosmery <rosm3rita@hotmail.com>
2018-09-26 17:28:45 -0500

    add styles

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
+
 register_nav_menus(array(
   'menu' => 'Menu ambientes',
 ));
+function add_myStyles(){
+ wp_enqueue_style('style',get_stylesheet_uri());
+// imprime directorio de la hoja de estilos
+ wp_enqueue_style('main',get_stylesheet_directory_uri() . '/css/main.css');
+ wp_enqueue_style('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+ wp_enqueue_style('carrousel','https://unpkg.com/flickity@2/dist/flickity.min.css');
+ wp_enqueue_script('jquery');
+ // wp_enqueue_script('bootstrapjs','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',array('jquery'),'3.3.6',true);
+wp_enqueue_script('bootstrapjs','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+wp_enqueue_script('carrouseljs','https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js');
+}
+
+// hook me permite meter mi codigo en wordpress
+add_action('wp_enqueue_scripts', 'add_myStyles');
 ?>
--- header.php
+++ header.php
@@ -2,19 +2,11 @@
     <html lang="en" dir="ltr">
     <head>
       <meta charset="utf-8">
-      <title></title>
-      <!-- <meta name="viewport" content="width=device-width"> -->
       <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-      <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
-      <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url')?>/css/main.css">
-      <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<?php wp_head(); ?>
+      <title></title>
+      <?php wp_head(); ?>
     </head>
     <body>
-      <?php $banner = wp_get_attachment_image_src(get_post_thumbnail_id(),'full'); ?>
-      <?php $banner = $banner[0]; ?>
-
         <section class="RenuevaTuCasa-content">
             <section class="RenuevaTuCasa-banner__wrapper">
                 <div class="letter">
@@ -29,12 +21,4 @@
                 </div>
             </section>
       <?php include(TEMPLATEPATH. '/menu.php');?>
-        </section>
-    </body>
-    </html>
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_url')?>/js/plugins.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_url')?>/js/core.min.js"></script>
+    </section>
